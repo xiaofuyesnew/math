04429e904eeff1cf1ef6385fd715271c1ee52a41
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-10-17 23:20:49 +0800

    update

--- src/util/alpha.js
+++ src/util/alpha.js
@@ -1,41 +1,70 @@
 export const randomCalc = (limit, num, arr = []) => {
   if (num) {
     const randomMethod = Math.random() < 0.5 ? 'plus' : 'minus'
+    const num1 = Math.round(Math.random() * limit)
+    const num2 = Math.round(Math.random() * limit)
     if (randomMethod === 'plus') {
-      const num1 = Math.round(Math.random() * limit)
-      const num2 = Math.round(Math.random() * limit)
-      const result = num1 + num2
-      arr.push({
-        method: randomMethod,
-        num1,
-        num2,
-        result,
-      })
-    } else {
-      const num1 = Math.round(Math.random() * limit)
-      const num2 = Math.round(Math.random() * limit)
-      if (num1 < num2) {
-        const result = num2 - num1
-        arr.push({
-          method: randomMethod,
-          num1: num2,
-          num2: num1,
-          result,
-        })
+      if (
+        arr.some(
+          (item) =>
+            item.num1 === num1 && item.num2 === num2 && item.method === 'plus'
+        )
+      ) {
+        return randomCalc(limit, num, arr)
       } else {
-        const result = num1 - num2
         arr.push({
-          method: randomMethod,
           num1,
           num2,
-          result,
+          result: num1 + num2,
         })
+        return randomCalc(limit, num - 1, arr)
+      }
+    } else {
+      if (num1 < num2) {
+        if (
+          arr.some(
+            (item) =>
+              item.num1 === num2 &&
+              item.num2 === num1 &&
+              item.method === 'minus'
+          )
+        ) {
+          return randomCalc(limit, num, arr)
+        } else {
+          arr.push({
+            method: randomMethod,
+            num1: num2,
+            num2: num1,
+            result: num2 - num1,
+          })
+          return randomCalc(limit, num - 1, arr)
+        }
+      } else {
+        if (
+          arr.some(
+            (item) =>
+              item.num1 === num1 &&
+              item.num2 === num2 &&
+              item.method === 'minus'
+          )
+        ) {
+          return randomCalc(limit, num, arr)
+        } else {
+          arr.push({
+            method: randomMethod,
+            num1,
+            num2,
+            result: num1 - num2,
+          })
+          return randomCalc(limit, num - 1, arr)
+        }
       }
     }
-    return randomCalc(limit, num - 1, arr)
   } else {
     return arr
   }
 }
+
+console.log(randomCalc(10, 10))
 
 export default randomCalc
