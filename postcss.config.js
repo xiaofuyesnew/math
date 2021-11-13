49d1e9d68eaeb89ef8e86a6f3ead93ecde845b10
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-11-13 23:47:23 +0800

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   plugins: {
-    autoprefixer: require('autoprefixer')
+    autoprefixer: require('autoprefixer'),
+    'postcss-px2vp': {
+      unitToConvert: 'px',
+      viewportWidth: 750,
+      unitPrecision: 4,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: ['van-'],
+      minPixelValue: 1,
+      mediaQuery: false,
+      replace: true,
+      exclude: [/\/src\/views\//],
+      // include: [/\/src\/mobile\//],
+      landscape: false,
+      landscapeUnit: 'vw',
+      landscapeWidth: 568,
+    },
   },
 }
---
+++ src/views/Select.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text">111</div>
+</template>
+
+
+<style lang="scss" scoped>
+.text {
+  font-size: 64px;
+}
+</style>
